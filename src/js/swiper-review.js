edf9626af89885a09d2bbfa6a8ce86e0215d209d
whitcr <whitcr0k@gmail.com>
2023-09-21 16:52:21 +0200

    Add swiper

---
+++ src/js/swiper-review.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  speed: 500,
+  autoheight: false,
+  spaceBetween: 32,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      initialSlide: 2,
+    },
+    768: {
+      slidesPerView: 2,
+      initialSlide: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+      initialSlide: 1,
+    },
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+});
