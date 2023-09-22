b9e78b66fafeed8fdd8e7740b18df5697c272398
Olek Myron <myrondell@gmail.com>
2023-09-22 22:52:58 +0300

    fixed js

--- src/js/teachers-modal.js
+++ src/js/teachers-modal.js
@@ -1,11 +1,13 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-teachers-open]'),
+    teachersRef: document.querySelectorAll('[data-modal-teachers-open]'),
     closeModalBtn: document.querySelector('[data-modal-teachers-close]'),
     modal: document.querySelector('[data-modal-teachers]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.teachersRef.forEach(teacher => {
+    teacher.addEventListener('click', toggleModal);
+  })
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
