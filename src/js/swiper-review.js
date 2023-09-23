57b3ba571c3571c3c0b4fa3366ae72c3daaab9e0
Maksym Zyza <mmziza88@gmail.com>
2023-09-23 20:50:32 +0300

    added optimize swiper-bundle.min.js

--- src/js/swiper-review.js
+++ src/js/swiper-review.js
@@ -1,26 +1,28 @@
-const swiper = new Swiper('.swiper', {
-  direction: 'horizontal',
-  speed: 500,
-  autoheight: false,
-  spaceBetween: 32,
+setTimeout(() => {
+  const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    speed: 500,
+    autoheight: false,
+    spaceBetween: 32,
 
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-      initialSlide: 2,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        initialSlide: 2,
+      },
+      768: {
+        slidesPerView: 2,
+        initialSlide: 2,
+      },
+      1280: {
+        slidesPerView: 3,
+        initialSlide: 1,
+      },
     },
-    768: {
-      slidesPerView: 2,
-      initialSlide: 2,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
     },
-    1280: {
-      slidesPerView: 3,
-      initialSlide: 1,
-    },
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    dynamicBullets: true,
-  },
-});
+  });
+}, 1000);
