b2daa2f43ff42450e1c25019c97014e088bf106c
Olek Myron <myrondell@gmail.com>
2023-09-23 17:03:39 +0300

    placed teachers data in separate js file

--- src/js/teachers-modal.js
+++ src/js/teachers-modal.js
@@ -1,51 +1,5 @@
 import manageScroll from './manage-body-scroll';
-
-const teachersHub = [
-  {
-    id: 1,
-    name: 'Maria',
-    rating: 2,
-    greeting: "Hello, I'm Maria",
-    position: "Your teacher in English for Beginners",
-    description:
-      'I will provide you with detailed feedback and additional learning tips for your language training. Post any questions you have in the chat. I or one of my colleagues will get back to you straight away.',
-    about:
-      'Soy de Barcelona, España. Me gusta mucho vivir en otros países para poder conocer mejor el país. Desde que terminé la universidad he estado trabajando de profesora de español y de catalán para extranjeros.',
-  },
-  {
-    id: 2,
-    name: 'Artem',
-    rating: 3,
-    greeting: "What's up, I'm Artem",
-    position: "Your trainer in American English",
-    description:
-      'With my innovative teaching techniques and a focus on interactive learning, I am excel in keeping you engaged and motivated',
-    about:
-      'Artem, lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate rem debitis odit, atque similique quia hic, eos voluptatem veritatis illum in. Explicabo exercitationem consectetur quos voluptate esse odio doloremque aut amet temporibus, non sint voluptatum necessitatibus',
-  },
-  {
-    id: 3,
-    name: 'Ilona',
-    rating: 4,
-    greeting: "Hello, I'm Ilona",
-    position: "Your tutor in English Advanced",
-    description:
-      'Specialising in advanced English, I provide intensive instruction, ensuring that you gain confidence in complex language tasks',
-    about:
-      'Ilona, lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate rem debitis odit, atque similique quia hic,  voluptatem veritatis illum in. Officiis neque, explicabo exercitationem consectetur quos voluptate esse odio doloremque aut amet temporibus, non sint voluptatum necessitatibus',
-  },
-  {
-    id: 4,
-    name: 'Volodymyr',
-    rating: 5,
-    greeting: "Hi, I'm Volodymyr",
-    position: "Your teacher in English for Business",
-    description:
-      'Expert in English for specific purposes, tailoring my approach to meet your individual career and industry-specific language needs',
-    about:
-      'Volodymyr, lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate rem debitis odit, atque similique quia hic. Officiis neque, explicabo exercitationem consectetur quos voluptate esse odio doloremque aut amet temporibus, non sint voluptatum necessitatibus',
-  },
-];
+import teachersHub from './teachers-data';
 
 (() => {
   const refs = {
